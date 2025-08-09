6ab2870eb4c14f2d83235f26223f93df67f6df84
Hayden Jones <hwjones90713@gmail.com>
2025-08-09 01:26:18 -0700

    finalized bot.py
    Finalized bot.py commands and added ledger database to gitignore

--- bot.py
+++ bot.py
@@ -21,4 +21,13 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 #INSERT COMMAND FUNCTIONALITY BELOW HERE
 #TODO: CREATE BETTING COMMANDS, LEDGER COMMANDS ON PER-SERVER BASIS, STATS PAGE OPT.
 
+from sidebet import setup as sidebet_setup
+from stats import setup as stats_setup
+
+@bot.event
+async def on_ready():
+    print(f"Bot online: {bot.user}")
+
+sidebet_setup(bot)
+stats_setup(bot)
 bot.run(discord_token)
