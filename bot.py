d9226e08fb5c759e30546ef077395eed9589b0ab
Hayden Jones <hwjones90713@gmail.com>
2025-08-08 23:38:09 -0700

    Create bot.py
    Made a template for bot.py, that successfully runs the bot and makes sure to create template for future functionality

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+import json
+import os
+
+from discord.ext import commands
+
+# Load API keys as arrays, grab each "value" as its key param
+
+def load_api_keys():
+    config_path = 'token.json'
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+        return config["APP_ID"], config["DISCORD_TOKEN"], config["PUBLIC_KEY"]
+    
+app_id, discord_token, public_key = load_api_keys()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+#INSERT COMMAND FUNCTIONALITY BELOW HERE
+#TODO: CREATE BETTING COMMANDS, LEDGER COMMANDS ON PER-SERVER BASIS, STATS PAGE OPT.
+
+bot.run(discord_token)
