6ff7e619f9c0e8f3f1b4c90aec90254e286fd83f
Hayden Jones <hwjones90713@gmail.com>
2025-08-08 23:47:45 -0700

    Update sidebet.py
    created ready environment for ledger.py, file that records and logs all sidebetting

--- sidebet.py
+++ sidebet.py
@@ -3,4 +3,6 @@ import discord
 from discord.ext import commands
 
 def setup(bot):
-    return True
+    @bot.command(name="sidebet")
+    async def bet(ctx, bettor1: discord.Member, bettor2: discord.Member, amount: int):
+        return
